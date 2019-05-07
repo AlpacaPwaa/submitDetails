0bab244a8e214a6d51ed2b502490cf3fc95d1aa1
AlpacaPwaa <47218224+AlpacaPwaa@users.noreply.github.com>
2019-05-07 19:27:59 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,3 @@
+function alertGen(){
+	alert(document.getElementById("name").value+" is a student at "+document.getElementById("school").value+" and is "+document.getElementById("age").value+" years old.");
+}
